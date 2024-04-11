4db9c7c291e5557a76b309201a04088fc59e2c88
A.J. Stein <alexander.stein@nist.gov>
2024-04-11 14:11:46 -0400

    Set up core fns and initial byte jump table

---
+++ cbor_python/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from typing import List
+from .initial_byte_table import *
+
+def loads(data: int|bytearray):
+    """Deserialize a CBOR data item with the generic data model in RFC 8949.
+    """
+    raise NotImplementedError("I cannot deserialize yet!")
+
+def dumps():
+    """Print out a representation of CBOB instance data.
+    """
+    raise NotImplementedError("I cannot print yet!")
