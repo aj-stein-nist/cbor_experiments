5dbb7a3ef7893599c6bce7ba4289a10119276897
A.J. Stein <alexander.stein@nist.gov>
2024-04-11 23:08:38 -0400

    Add some primitive tag types

--- cbor_python/tag_table.py
+++ cbor_python/tag_table.py
@@ -4,3 +4,9 @@
 # See CSV of current IANA CBOR item tag registry at URL below.
 # https://www.iana.org/assignments/cbor-tags/cbor-tags.xhtml
 
+TEXT_STRING_TAG = 0
+INTEGER_FLOAT_TAG = 1
+BYTE_STRING_UNSIGNED_BIGNUM_TAG = 2
+BYTE_STRING_NEGATIVE_BIGNUM_TAG = 3
+ARRAY_DECIMAL_FRACTION_TAG = 4
+ARRAY_BIG_FLOAT_TAG = 5
